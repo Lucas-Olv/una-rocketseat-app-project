888f652ede5a53c747e5e28fee354b421a937c38
Lucas Lopes de Oliveira <lucas.lopes6588@gmail.com>
2022-05-26 21:35:19 -0300

    Landing page layout done.

---
+++ pages/createAccount.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/createAccount.module.scss';
+
+const createAccount: NextPage = () => {
+  return(
+    <section></section>
+  )
+}
+
+export default createAccount;
